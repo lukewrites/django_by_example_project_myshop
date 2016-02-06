f0c4f462a1e7fc24542294d6b02640d9ac568179
Luke Petschauer <luke.petschauer@gmail.com>
2016-02-06 11:27:49 -0800

    working on models

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Category, Product
 
-# Register your models here.
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
