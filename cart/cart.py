41959cc28ccb84c72f3387bce4c8bd350a2018f2
Luke Petschauer <luke.petschauer@gmail.com>
2016-02-11 22:02:41 -0800

    building the cart

--- cart/cart.py
+++ cart/cart.py
@@ -6,12 +6,28 @@ from shop.models import Product
 class Cart(object):
 
     def __init__(self, request):
-        """
-        Initialize the cart.
-        """
+        """ Initialize the cart. """
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
             # save an empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """ Add a product to the cart or update its quantity. """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        # update the session cart!
+        self.session[settings.CART_SESSION_ID] = self.cart
+        # mark the session as 'modified' to make sure it's saved.
+        self.session.modified = True
